938b2f1d25b9af30d851f223d7e0d2777857a186
mouralipe <felipemoura92@hotmail.com>
2021-05-20 17:23:02 -0300

    finish challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,48 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if(newTaskTitle === '') {
+      return;
+    }
+
+    setTasks([...tasks, {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }]);
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+    const allTasks = [...tasks];
+
+    const changedTasks = allTasks.map(task => {
+      if (task.id === id) {
+        return {
+          id: task.id,
+          title: task.title,
+          done: !task.done
+        }
+      } else {
+        return task;
+      }
+    })
+
+    setTasks(changedTasks);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+
+    const allTasks = [...tasks];
+
+    const changedTasks = allTasks.filter(task => task.id !== id);
+
+    setTasks(changedTasks);
   }
 
   return (
